44a1db3df27e58b646cf583fc62ff83600bcce2c
amine alaoui <omari1amineco@gmail.com>
2023-12-28 23:53:23 +0100

    fix: index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import MainContainer from "@/components/MainContainer";
 import { Inter } from "next/font/google";
-import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
