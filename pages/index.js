317a83cca381062afbfc92c1491266817caabfed
amine alaoui <omari1amineco@gmail.com>
2023-12-31 13:58:52 +0100

    remove useless imports in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import MainContainer from "@/components/MainContainer";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
